4c32f0e9fe0c1a2dfa1cdd8f4193308f11b58f1c
tahathedancer98 <bookingtaha98@gmail.com>
2021-11-14 14:02:11 +0100

    _black source code

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -209,7 +209,7 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2021 Copyright:
-        <a class="text-white" href="https://github.com/tahathedancer98/ProjetLaravelM1/tree/master">CODE SOURCE</a>
+        <a class="text-white" href="https://github.com/tahathedancer98/ProjetLaravelM1/tree/master" target="_blank">CODE SOURCE</a>
     </div>
     <!-- Copyright -->
 </footer>
